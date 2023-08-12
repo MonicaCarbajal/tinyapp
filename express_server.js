a11bc54c9b4e12b497206f34cf46bc582c135e4b
Monica Carbajal <monica.carbajal2@gmail.com>
2023-08-12 11:20:12 -0600

    addign html in url catcher

--- express_server.js
+++ express_server.js
@@ -9,6 +9,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -20,6 +25,8 @@ app.get("/hello", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
