dd89610d06ac51838a6f162afb770e7c0f20e515
Monica Carbajal <monica.carbajal2@gmail.com>
2023-08-12 11:51:13 -0600

    addign nightvale URL

--- express_server.js
+++ express_server.js
@@ -6,13 +6,19 @@ app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  // "08j7cK": "http://http://www.nightvalepresents.com/"
 };
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: "http://http://www.nightvalepresents.com/" };
+  res.render("urls_show", templateVars);
+}); 
 
 app.get("/", (req, res) => {
   res.send("Hello!");
