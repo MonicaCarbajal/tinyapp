498771fc8e3f8e6f25c1f33125337f18086b7c83
Monica Carbajal <monica.carbajal2@gmail.com>
2023-08-13 12:45:58 -0600

    adding logout feature

--- express_server.js
+++ express_server.js
@@ -101,6 +101,13 @@ app.post("/login", (req, res) => {
   }
 });
 
+app.post("/logout", (req, res) => {
+  // Clear the username cookie
+  res.clearCookie("username");
+
+  // Redirect back to the /urls page
+  res.redirect("/urls");
+});
 
 
 function generateRandomString() {
