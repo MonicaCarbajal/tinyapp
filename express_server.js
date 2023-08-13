eb48583c0f15dcd1218254010c060b2d933c7b6e
Monica Carbajal <monica.carbajal2@gmail.com>
2023-08-12 20:02:49 -0600

    Capturing URL and displaying urls for real this time

--- express_server.js
+++ express_server.js
@@ -26,6 +26,15 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.status(404).send("Short URL not found");
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
