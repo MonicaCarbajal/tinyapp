7d924dec2824a2949a36befc952e96b065c09c1f
Monica Carbajal <monica.carbajal2@gmail.com>
2023-08-13 12:13:05 -0600

    adding username is all pages

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require("cookie-parser");
 
 app.set("view engine", "ejs");
 
@@ -10,10 +11,11 @@ const urlDatabase = {
 };
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+app.use((req, res, next) => {
+  res.locals.username = req.cookies["username"];
+  next();
 });
 
 app.get("/urls/new", (req, res) => {
@@ -45,6 +47,14 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -90,6 +100,8 @@ app.post("/login", (req, res) => {
     res.status(400).send("Invalid username");
   }
 });
+
+
 
 function generateRandomString() {
   const length = 6;
